ab1629055ac07177fe6d5284bb65829146c9d035
kvelenis <kvelenis@gmail.com>
2022-11-17 20:04:02 +0200

    my message is this

--- accordion_1/accordion_1.js
+++
@@ -1,8 +0,0 @@
-var $title = $('.js-title');
-var copy   = '.js-copy';
-
-$title.click(function () {
-  $(this).next(copy).slideToggle();
-  $(this).parent().siblings().children().next().slideUp();
-  return false;
-});
